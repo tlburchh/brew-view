5181a348f721fc3370df079902f7a478b70857f6
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-12-31 10:31:40 -0500

    working on toggling availability and updating database

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,83 +1,67 @@
 import React from "react";
 // import ReactDOM from "react-dom";
+// import API from "../../utils/API"
 import MUIDataTable from "mui-datatables";
 
 class Admin extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      data: this.props.data,
+      available:false
     };
-
-    
+  console.log(this.state.data, "Admin State Data")
   }
+
+  
   render() {
     const columns = ["Brewery", "Beer Name", "Beer Style", "ABV", "IBU", "On Tap"];
 
-    const data = this.props.data.map(beer => {
+    const data = this.state.data.map(beer => {
       return [`${beer['Brewery Name']}`,` ${beer['Beer Name']}`,` ${beer['Beer Style']}`,` ${beer.ABV}`,` ${beer.IBU}`,` ${beer.available}` ]
     
 
     });
 
-    //   ["Gabby George", "Business Analyst", "Minneapolis", 30, "$100,000"],
-    //   ["Aiden Lloyd", "Business Consultant", "Dallas", 55, "$200,000"],
-    //   ["Jaden Collins", "Attorney", "Santa Ana", 27, "$500,000"],
-    //   ["Franky Rees", "Business Analyst", "St. Petersburg", 22, "$50,000"],
-    //   ["Aaren Rose", "Business Consultant", "Toledo", 28, "$75,000"],
-    //   [
-    //     "Blake Duncan",
-    //     "Business Management Analyst",
-    //     "San Diego",
-    //     65,
-    //     "$94,000"
-    //   ],
-    //   ["Frankie Parry", "Agency Legal Counsel", "Jacksonville", 71, "$210,000"],
-    //   ["Lane Wilson", "Commercial Specialist", "Omaha", 19, "$65,000"],
-    //   ["Robin Duncan", "Business Analyst", "Los Angeles", 20, "$77,000"],
-    //   ["Mel Brooks", "Business Consultant", "Oklahoma City", 37, "$135,000"],
-    //   ["Harper White", "Attorney", "Pittsburgh", 52, "$420,000"],
-    //   ["Kris Humphrey", "Agency Legal Counsel", "Laredo", 30, "$150,000"],
-    //   ["Frankie Long", "Industrial Analyst", "Austin", 31, "$170,000"],
-    //   ["Brynn Robbins", "Business Analyst", "Norfolk", 22, "$90,000"],
-    //   ["Justice Mann", "Business Consultant", "Chicago", 24, "$133,000"],
-    //   [
-    //     "Addison Navarro",
-    //     "Business Management Analyst",
-    //     "New York",
-    //     50,
-    //     "$295,000"
-    //   ],
-    //   ["Jesse Welch", "Agency Legal Counsel", "Seattle", 28, "$200,000"],
-    //   ["Eli Mejia", "Commercial Specialist", "Long Beach", 65, "$400,000"],
-    //   ["Gene Leblanc", "Industrial Analyst", "Hartford", 34, "$110,000"],
-    //   ["Danny Leon", "Computer Scientist", "Newark", 60, "$220,000"],
-    //   ["Lane Lee", "Corporate Counselor", "Cincinnati", 52, "$180,000"],
-    //   ["Jesse Hall", "Business Analyst", "Baltimore", 44, "$99,000"],
-    //   ["Danni Hudson", "Agency Legal Counsel", "Tampa", 37, "$90,000"],
-    //   ["Terry Macdonald", "Commercial Specialist", "Miami", 39, "$140,000"],
-    //   ["Justice Mccarthy", "Attorney", "Tucson", 26, "$330,000"],
-    //   ["Silver Carey", "Computer Scientist", "Memphis", 47, "$250,000"],
-    //   ["Franky Miles", "Industrial Analyst", "Buffalo", 49, "$190,000"],
-    //   ["Glen Nixon", "Corporate Counselor", "Arlington", 44, "$80,000"],
-    //   [
-    //     "Gabby Strickland",
-    //     "Business Process Consultant",
-    //     "Scottsdale",
-    //     26,
-    //     "$45,000"
-    //   ],
-    //   ["Mason Ray", "Computer Scientist", "San Francisco", 39, "$142,000"]
-    // ];
 
     const options = {
       filterType: "dropdown",
       responsive: "scroll",
-    
+      selectableRows: true,
+      rowsPerPage:20,
+
+      onRowClick: (rowData, rowState) => {
+
+
+        const beerAvailable= rowData[5]
+
+        // const available = this.state.data.available
+
+        if(beerAvailable === false){
+         return (
+          beerAvailable=== true,
+          this.setState({
+            beerAvailable: true,
+            // available: true
+           })
+         )
+        }else if( beerAvailable === true) {
+          return( 
+            beerAvailable=== false,
+            this.setState({
+              beerAvailable: false,
+              // available: false
+          })
+          )
+        }
+
+        console.log(rowData[5], rowState);
+      }
     };
 
     return (
       <MUIDataTable
-        title={"ACME Employee list"}
+        title={"Available Beers"}
         data={data}
         columns={columns}
         options={options}
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,9 @@ export default {
 
   getBeers: function () {
     return axios.get("/api/beers");
-  }
+  },
+  // updateAvailable: function (updatedBeer) {
+  //   return axios.put("/api/beers/" + id, updatedBeer );
+  // }
 }
 console.log('axios check')
